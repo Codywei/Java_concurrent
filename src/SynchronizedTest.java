1e3ca42deacbc59d1000fcad6c550c697e806110
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-24 16:06:16 +0800

    synchronized 关键字

--- src/SynchronizedTest.java
+++ src/SynchronizedTest.java
@@ -1,21 +1,10 @@
 import java.util.ArrayList;
-
+/**
+ synchronized 关键字
+ * */
 public class SynchronizedTest {
     public static void main(String[] args)  {
         final InsertData insertData = new InsertData();
-
-//        new Thread() {
-//            public void run() {
-//                insertData.insert(Thread.currentThread());
-//            };
-//        }.start();
-//
-//
-//        new Thread() {
-//            public void run() {
-//                insertData.insert(Thread.currentThread());
-//            };
-//        }.start();
 
         new Thread(){
             @Override
