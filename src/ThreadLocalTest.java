87ab3cffbdf43e7884ff18a51b26454e86187730
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-24 17:06:36 +0800

    ThreadLocal练习

--- src/ThreadLocalTest.java
+++ src/ThreadLocalTest.java
@@ -2,6 +2,8 @@
  * ThreadLocal练习
  * ThreadLocal，很多地方叫做线程本地变量，也有些地方叫做线程本地存储。
  * ThreadLocal为变量在每个线程中都创建了一个副本，那么每个线程可以访问自己内部的副本变量。
+ *
+ * 在数据库连接中，若connect为共享变量将影响线程安全，而同步处理会影响效率，所以常见方法是采用ThreadLocal
  * */
 public class ThreadLocalTest {
     ThreadLocal<Long> longLocal = new ThreadLocal<Long>();
