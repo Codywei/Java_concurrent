faed94251a70f7ab202e224f415e016d2eda93e0
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-24 16:30:24 +0800

    读写锁的使用

--- src/LockTest3.java
+++ src/LockTest3.java
@@ -1,5 +1,7 @@
 /**
- * 　读写锁将对一个资源（比如文件）的访问分成了2个锁，一个读锁和一个写锁。
+     读写锁的使用
+
+   　读写锁将对一个资源（比如文件）的访问分成了2个锁，一个读锁和一个写锁。
 
  　　正因为有了读写锁，才使得多个线程之间的读操作不会发生冲突。
 
@@ -12,6 +14,7 @@
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class LockTest3 {
+    //如果锁具备可重入性，则称作为可重入锁。像synchronized和ReentrantLock都是可重入锁，可重入性实际上表明了锁的分配机制：基于线程的分配，而不是基于方法调用的分配。
     private ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
 
     public static void main(String[] args)  {
