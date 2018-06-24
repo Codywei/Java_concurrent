4f58672a698cd0ea9ad95b1039eff0350d01f5d4
CodyWei <33363378+Codywei@users.noreply.github.com>
2018-06-24 16:51:52 +0800

    votaile关键字练习

--- src/VotaileTest.java
+++ src/VotaileTest.java
@@ -1,5 +1,7 @@
 /**
- * votaile关键字练习，程序结果小于10000，因为votaile虽能保证操作的可见性，但不能保证操作原子性
+ votaile关键字练习，程序结果小于10000，因为votaile虽能保证操作的可见性，但不能保证操作原子性
+
+ votaile两种应用场景：1.标记状态量 2.双检锁
  * */
 
 public class VotaileTest {
@@ -14,15 +16,17 @@ public class VotaileTest {
         for(int i=0;i<10;i++){
             new Thread(){
                 public void run() {
-                    for(int j=0;j<1000;j++)
+                    for(int j=0;j<1000;j++) {
                         test.increase();
+                    }
                     System.out.println(test.inc);
                 };
             }.start();
         }
-
-       while(Thread.activeCount()>1)  //保证前面的线程都执行完
+      //保证前面的线程都执行完
+       while(Thread.activeCount()>1) {
            Thread.yield();
+       }
         System.out.println(test.inc);
     }
 }
